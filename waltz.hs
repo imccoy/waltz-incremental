ade80d8345f513c72387886973086d3b6475b41a
iain <iain@mccoy.id.au>
2011-08-08 16:34:29 +1000

    move to a world where not everything is a value object

--- waltz.hs
+++ waltz.hs
@@ -11,8 +11,7 @@ import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString as B
 
 -- TODOS
--- How do you make two different haps using two different columns of a html table?
--- How do you make two different haps that submit at the same time? Two different haps that submit independently?
+-- How do you make two different actionreceivers using two different columns of a html table?
 
 
 -- framework stuff
@@ -22,9 +21,16 @@ import Debug.Trace
 -- framework: page display
 follow_to source target = FuncAssociationWalk source target
 showValueAsString = FuncF (\(ValueEntity entity) -> Text $ show entity)
-showEntryText = FuncF getText
-  where getText (ValueEntity (EntityEntry (Entry text))) = Text text
-        getText e = error $ "can't get text " ++ (show e)
+showSectionText = FuncE getText
+  where getText dta (ValueEntity entity) = case (entityValueItem dta entity) of
+                                             EntitySection (Section text _) -> Text text
+                                             e                              -> error $ "can't get text " ++ (show e)
+        getText dta e = error $ "can't get text " ++ (show e)
+showEntryText = FuncE getText
+  where getText dta (ValueEntity entity) = case (entityValueItem dta entity) of
+                                             EntityEntry (Entry text) -> Text text
+                                             e                        -> error $ "can't get text " ++ (show e)
+        getText dta e = error $ "can't get text " ++ (show e)
 
 
 data Value = Tag String [(String, String)] Value
@@ -36,7 +42,7 @@ data Value = Tag String [(String, String)] Value
            | ActionReceiverInput ActionReceiverField ActionReceiverControl
   deriving (Show)
 
-data Func = FuncAssociationWalk String String | FuncMap (Value -> Value) | FuncF (Value -> Value)
+data Func = FuncAssociationWalk String String | FuncEntityWhere (EntityValue -> Bool) | FuncMap (Value -> Value) | FuncF (Value -> Value) | FuncE (EntityData -> Value -> Value)
 
 instance Show Func where
   show (FuncAssociationWalk source target) = "func: " ++ source ++ " -> " ++ target
@@ -81,30 +87,50 @@ drawPage val dta causingAction = drawPageValue val 0
         invoke' (FuncAssociationWalk source_type attr) vs = error $ "Can't walk association over " ++ (show vs)
         invoke' (FuncMap f) [Values vs] = Values $ map f vs
         invoke' (FuncMap f) vs = Values $ map f vs
+        invoke' (FuncEntityWhere f) [] = ValueEntity $ entityWithValue dta f
         invoke' (FuncF f) [v] = f v
         invoke' (FuncF f) [] = error "No arguments to singular function"
         invoke' (FuncF f) _ = error "Too many arguments to singular function"
+        invoke' (FuncE f) [v] = f dta v
+        invoke' (FuncE f) [] = error "No arguments to singular entity function"
+        invoke' (FuncE f) _ = error "Too many arguments to singular entity function"
 
 
 
 
 -- framework: data store
 
-type EntityData = [EntityStore]
-emptyData = []
-data EntityStore = EntityStoreEntry Entity Entity
+data EntityData = EntityData Integer [EntityRelationship] [EntityValueItem]
+emptyData = EntityData 0 [] []
+data EntityRelationship = EntityRelationship Entity Entity
+data EntityValueItem = EntityValueItem Entity EntityValue
 
-build_data :: [EntityData -> EntityData] -> EntityData
-build_data fs = build_data' emptyData fs
-  where build_data' d [] = d
-        build_data' d (f:fs) = build_data' (f d) fs
+data Entity = Entity Integer
+  deriving (Show, Eq)
 
-add_entity parent child d = (EntityStoreEntry parent child):d
+build_data :: EntityData -> [EntityData -> EntityData] -> EntityData
+build_data d [] = d
+build_data d (f:fs) = build_data (f d) fs
+
+new_entity value (EntityData n rs vs) = (entity, EntityData (n+1) rs ((EntityValueItem entity value):vs))
+  where entity = Entity $ n+1
+add_entity parent child dta = let (entity, dta') = new_entity child dta
+                               in case dta' of EntityData n rs vs -> EntityData n ((EntityRelationship parent entity):rs) vs
 
 lookup_related :: EntityData -> Entity -> [Entity]
-lookup_related dta entity = map (\(EntityStoreEntry a b) -> b) $ (filter matchesEntity) dta
-  where matchesEntity :: EntityStore -> Bool
-        matchesEntity (EntityStoreEntry a b) = a == entity
+lookup_related (EntityData _ dta _) entity = map (\(EntityRelationship a b) -> b) $ (filter matchesEntity) dta
+  where matchesEntity (EntityRelationship a b) = a == entity
+
+entityValueItem :: EntityData -> Entity -> EntityValue
+entityValueItem (EntityData _ _ dta) entity = (\(EntityValueItem a b) -> b) $ head $ (filter matchesEntity) dta
+  where matchesEntity (EntityValueItem a _) = a == entity
+
+entityWithValue :: EntityData -> (EntityValue -> Bool) -> Entity
+entityWithValue (EntityData _ _ dta) matcher = case (filter matchesValue) dta of
+                                                 ((EntityValueItem a b):_) -> a
+                                                 otherwise -> error "no entity with matching value"
+                                               where matchesValue (EntityValueItem _ v) = matcher v
+
 
 -- framework: user input
 
@@ -131,7 +157,7 @@ app db request = do current_db <- tryIO $ readIORef db
                     request_body_chunks <- EL.consume
                     let request_body_query = parseQuery $ B.concat request_body_chunks
                     let action = processRequest request current_db request_body_query
-                    let actions = actionsFor action
+                    let actions = actionsFor current_db action
                     let (response, new_db) = processActions action actions current_db
                     tryIO $ writeIORef db new_db
                     return response
@@ -151,10 +177,9 @@ actionReceiverToAction request_body_query = let action_receiver_name = lastInQue
                                                 getter action_receiver_field = lastInQueryString request_body_query $ fieldName action_receiver_field
                                              in case validityErrors action_receiver getter of
                                                   []     -> (actionReceiverBuilder action_receiver) getter
-                                                  errors -> (last_action `actionWith` action_receiver) errors
+                                                  errors -> (last_action `actionWith` action_receiver) -- include errors too, somehow
 
-actionWith :: Action -> ActionReceiver -> [(ActionReceiverField,String)] -> Action
-actionWith a receiver errors = a
+actionWith a receiver = a
 
 validityErrors (ActionReceiver _ fields _) getter = errors fields
   where errors (field:fields) = fieldErrors field (validators field) $ errors fields
@@ -185,66 +210,74 @@ main = do
 
 -- application stuff
 
-data Section = Good | Bad | Confusing
+data SectionMood = Good | Bad | Confusing
+  deriving (Show, Read, Eq)
+data Section = Section String SectionMood
   deriving (Show, Read, Eq)
 data Entry = Entry String
   deriving (Show, Read, Eq)
 
-data Entity = EntitySection Section | EntityEntry Entry
+data EntityValue = EntitySection Section | EntityEntry Entry
   deriving (Show, Read, Eq)
 
-data Action = AddPropertyOn Entity Entity | ShowPage Value | NewEntry Section String | ShowRetro Section
+hasSectionMood desired_mood (EntitySection section@(Section _ mood)) = desired_mood == mood
+hasSectionMood desired_mood section = False
+
+data Action = AddPropertyOn Entity EntityValue | ShowPage Value | NewEntry SectionMood String | ShowRetro
 
 readStringPair :: String -> (String, String)
 readStringPair string = read string
 
 instance Show Action where
   show (NewEntry section string) = show ("NewEntry", show (section, string))
-  show (ShowRetro section) = show ("ShowRetro", show section)
+  show (ShowRetro) = show ("ShowRetro", "")
 
 instance Read Action where
   readsPrec _ string = let action = case readStringPair string of
                                       ("NewEntry", info)  -> let (section, string) = read info
                                                              in NewEntry section string
-                                      ("ShowRetro", info) -> let section = read info
-                                                             in ShowRetro section
+                                      ("ShowRetro", info) -> ShowRetro
                         in [(action, "")]
 
 
-defaultAction = ShowRetro Good
+defaultAction = ShowRetro
 
-actionsFor (ShowRetro section) = [ShowPage $ retro_page section]
-actionsFor (NewEntry section text) = [AddPropertyOn (EntitySection section) (EntityEntry $ Entry text), ShowPage $ retro_page section]
+actionsFor dta (ShowRetro) = [ShowPage $ retro_page]
+actionsFor dta (NewEntry section text) = [AddPropertyOn (entityWithValue dta (hasSectionMood section)) (EntityEntry $ Entry text),
+                                          (ShowPage retro_page) `actionWith` section]
 
 possibleSectionStrings = ["Good", "Bad", "Confusing"]
 
-newEntryActionReceiverSection = ActionReceiverField { fieldName = "section", validators = [EnsureIsOneOf possibleSectionStrings] }
+-- this process has waaaaaay too much boilerplate
+newEntryActionReceiverSectionMood = ActionReceiverField { fieldName = "sectionMood", validators = [EnsureIsOneOf possibleSectionStrings] }
 newEntryActionReceiverText = ActionReceiverField { fieldName = "text", validators = [EnsureIsNotEmpty] }
-newEntryActionReceiver = ActionReceiver "newEntry" [newEntryActionReceiverSection, newEntryActionReceiverText] (\getter -> NewEntry (read $ getter (newEntryActionReceiverSection)) (getter newEntryActionReceiverText))
+newEntryActionReceiver = ActionReceiver "newEntry" [newEntryActionReceiverSectionMood, newEntryActionReceiverText] (\getter -> NewEntry (read $ getter (newEntryActionReceiverSectionMood)) (getter newEntryActionReceiverText))
 
 allActionReceivers = [newEntryActionReceiver]
-
 
 retro_entry entry = Tag "span" [] $ ValueFuncCall showEntryText [entry]
 
 retro_entries section = Tag "div" [] $ Values [
-                                      (Tag "h2" [] $ ValueFuncCall showValueAsString [section]),
+                                      (Tag "h2" [] $ ValueFuncCall showSectionText [section]),
 				      (ValueFuncCall (FuncMap retro_entry) [ValueFuncCall ("Section" `follow_to` "Entry") [section]])]
 
-retro = Tag "div" [] $ Values [retro_entries(ValueEntity $ EntitySection Good),
-                               retro_entries(ValueEntity $ EntitySection Bad),
-                               retro_entries(ValueEntity $ EntitySection Confusing)]
+retro_view = Tag "div" [] $ Values [retro_entries (ValueFuncCall (FuncEntityWhere $ hasSectionMood Good) []),
+                                    retro_entries (ValueFuncCall (FuncEntityWhere $ hasSectionMood Bad) []),
+                                    retro_entries (ValueFuncCall (FuncEntityWhere $ hasSectionMood Confusing) [])]
            
-sample_data = build_data
+sample_data = let (good, dta) = new_entity (EntitySection $ Section "Good" Good) emptyData
+                  (bad, dta') = new_entity (EntitySection $ Section "Bad" Bad) dta
+                  (confusing, dta'') = new_entity (EntitySection $ Section "Ugly" Confusing) dta'
+               in build_data dta''
                      [
-                      add_entity (EntitySection Good) (EntityEntry $ Entry "It's Okay"),
-                      add_entity (EntitySection Bad) (EntityEntry $ Entry "It's Ugly")
+                      add_entity good (EntityEntry $ Entry "It's Okay"),
+                      add_entity bad (EntityEntry $ Entry "It's Ugly")
                       ]
 
-retro_page initial_section = Values [(Tag "h1" [] $ Text "Retro"),
-                                     retro,
+retro_page = Values [(Tag "h1" [] $ Text "Retro"),
+                                     retro_view,
                                      ActionReceiverValue newEntryActionReceiver [
-                                       ActionReceiverInput newEntryActionReceiverSection (DropdownV possibleSectionStrings $ show initial_section),
+                                       ActionReceiverInput newEntryActionReceiverSectionMood $ Dropdown possibleSectionStrings,
                                        ActionReceiverInput newEntryActionReceiverText Textfield
                                      ]]
 
