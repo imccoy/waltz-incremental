61bf939f19b83c731183057c5c0d37a979f11f4d
Iain McCoy <iain@mccoy.id.au>
2011-12-13 13:41:47 +1100

    Fix the broken

--- Bprime.main.hs
+++ Bprime.main.hs
@@ -2,4 +2,4 @@ import B
 
 main = do
   putStrLn $ show $ words_length ["abcdef", "abc"]
-  putStrLn $ show $ words_length1rlqZC (words_length ["abc"]) "abcdef"
+  putStrLn $ show $ words_length1rlpZC (words_length ["abc"]) "abcdef"
