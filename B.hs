e6fc1d8d52098f561694687c5576fce043a98ea4
Iain McCoy <iain@mccoy.id.au>
2011-12-14 12:41:01 +1100

    test code for stage 2: invocation

--- B.hs
+++ B.hs
@@ -1,7 +1,6 @@
 module B where
 
--- words_length [] = 0
--- words_length (w:ws) = (length w) + (words_length ws)
-
 words_length [] = 0
-words_length (w:ws) = (words_length ws) + (length w)
+words_length (w:ws) = (length w) + (words_length ws)
+
+words_length_doubled x = 2 * words_length x
