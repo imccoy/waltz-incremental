3c8cab3f4d8e5cad9d677bd1bc71fd9d961bec06
Iain McCoy <iain@mccoy.id.au>
2012-03-09 01:32:12 +1100

    mighty update-foo

--- DatabaseApplier.hs
+++ DatabaseApplier.hs
@@ -43,34 +43,41 @@ db_strategy_tdef tdefs tdef@(Data qTcon tbinds cdefs) = Hs.HsTuple [name, db_str
         type_name' (Tapp a b) = type_name' a
         type_name' _ = ""
 
-typeclass_instances_tdefs = map typeclass_instances_tdef
-typeclass_instances_tdef (Data qTcon tbinds cdefs) = Hs.HsInstDecl hs_nowhere context (Hs.UnQual$ Hs.HsIdent "DbIncrementalised") types [decl]
-  where 
-        incrementalise_type_con = Hs.HsTyVar $ Hs.HsIdent $ snd $ incrementalise_name qTcon
-        base_type_con = Hs.HsTyVar $ Hs.HsIdent $ snd $ qTcon
-        base_type = foldl Hs.HsTyApp base_type_con (map (\(tvar, kind) -> Hs.HsTyVar $ Hs.HsIdent tvar) tbinds)
-        incrementalise_type = foldl Hs.HsTyApp incrementalise_type_con (map (\(tvar, kind) -> Hs.HsTyVar $ Hs.HsIdent tvar) $ tbinds ++ (mutant_tbinds tbinds))
-        context = map (\(tvar, kind) -> (Hs.UnQual $ Hs.HsIdent "DbIncrementalised", [Hs.HsTyVar $ Hs.HsIdent tvar])) (mutant_tbinds tbinds)
-        types = [incrementalise_type]
-        decl = Hs.HsFunBind matches
-        matches = (map (applyDbInputChangeCdef qTcon) cdefs) -- ++ (concat $ map (applyDbInputChangeBuild qTcon) cdefs)
+typeclass_instances_tdefs tdefs = concat $ [db_incrementalised_instances_tdefs tdefs, db_initialised_instances_tdefs tdefs]
 
-applyDbInputChangeCdef qTcon (Constr qDcon tbinds tys) = Hs.HsMatch hs_nowhere (Hs.HsIdent "applyDbInputChange") pat (Hs.HsUnGuardedRhs exp) []
+db_incrementalised_instances_tdefs = map db_incrementalised_instances_tdef
+db_incrementalised_instances_tdef tdef@(Data qTcon tbinds cdefs) = typeclass_instance "DbIncrementalised" (mutant_tbinds tbinds) tdef incrementalise_name (tbinds ++ (mutant_tbinds tbinds)) applyDbInputChangeCdef
+
+db_initialised_instances_tdefs = map db_initialised_instances_tdef
+db_initialised_instances_tdef tdef@(Data qTcon tbinds cdefs) = typeclass_instance "DbInitialise" tbinds tdef id tbinds setInitialValueCdef
+
+typeclass_instance name deps (Data qTcon tbinds cdefs) type_name_transformation all_tbinds method_builder = Hs.HsInstDecl hs_nowhere context (Hs.UnQual$ Hs.HsIdent name) [type_] [decl]
+  where 
+        type_con = (Hs.HsTyVar $ Hs.HsIdent $ snd $ type_name_transformation qTcon)
+        type_ = foldl Hs.HsTyApp type_con (map (\(tvar, kind) -> Hs.HsTyVar $ Hs.HsIdent tvar) all_tbinds)
+        context = map (\(tvar, kind) -> (Hs.UnQual $ Hs.HsIdent name, [Hs.HsTyVar $ Hs.HsIdent tvar])) deps
+        decl = Hs.HsFunBind $ (map method_builder cdefs) -- ++ (concat $ map (applyDbInputChangeBuild qTcon) cdefs)
+
+
+
+applyDbInputChangeCdef (Constr qDcon tbinds tys) = Hs.HsMatch hs_nowhere (Hs.HsIdent "applyDbInputChange") pat (Hs.HsUnGuardedRhs exp) []
   where (input_change_pat, input_change_pat_arg_names) = hs_pat_names (incrementalise_name qDcon) "_change" (length tys)
-        pat = [input_change_pat, Hs.HsPVar $ Hs.HsIdent "address"]
+        pat = [Hs.HsPVar $ Hs.HsIdent "handle", Hs.HsPVar $ Hs.HsIdent "structure", input_change_pat, Hs.HsPVar $ Hs.HsIdent "address"]
         exp = Hs.HsDo $ map Hs.HsQualifier (invocations ++ [Hs.HsApp (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "return") Hs.unit_con])
         invocations = zipWith invocation input_change_pat_arg_names [0..]
         invocation change_n n = Hs.HsParen $ Hs.HsApp 
-                                 (Hs.HsApp 
-                                   (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "applyDbInputChange")
+                                 (Hs.HsApp
+                                   (Hs.HsApp
+                                     (Hs.HsApp 
+                                       (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "applyDbInputChange")
+                                       (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "handle"))
+                                     (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "structure"))
                                    (Hs.HsVar $ Hs.UnQual change_n))
                                  (new_address qDcon n)
         new_address constructor position = Hs.HsParen $ Hs.HsApp (Hs.HsApp (Hs.HsApp (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "appendAddress")
                                                                                      (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "address")) 
                                                                            (Hs.HsLit $ Hs.HsString $ snd constructor))
                                                                  (Hs.HsLit $ Hs.HsInt $ position)
-        make_bind a b = Hs.HsParen (Hs.HsApp (Hs.HsApp (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "(>>)") a)
-                                             b)
 {-
  -
  - applyDbInputChangeBuild qTcon (Constr qDcon tbinds tys) = generate_cdef_builds qTcon tys builder
@@ -82,6 +89,21 @@ applyDbInputChangeCdef qTcon (Constr qDcon tbinds tys) = Hs.HsMatch hs_nowhere (
  -                arg_names = (arg_names_before ++ (Hs.HsIdent "base"):arg_names_after)
  -                (arg_names_before, arg_names_after) = splitAt (length tys1) input_change_pat_arg_names
  -}
+
+
+setInitialValueCdef (Constr qDcon tbinds tys) = Hs.HsMatch hs_nowhere (Hs.HsIdent "setInitialValue") pat (Hs.HsUnGuardedRhs exp) []
+  where (base_pat, base_pat_arg_names) = hs_pat_names (qDcon) "_base" (length tys)
+        pat = [Hs.HsPVar $ Hs.HsIdent "handle", Hs.HsPVar $ Hs.HsIdent "structure", base_pat]
+        exp = Hs.HsApp (Hs.HsApp (app_with_handle_and_structure "setInitialValue'")
+                                 (Hs.HsLit $ Hs.HsString $ snd qDcon))
+                       invocations
+        invocations = Hs.HsList $ map (invocation) base_pat_arg_names
+        invocation name = Hs.HsApp (app_with_handle_and_structure "setInitialValue") (Hs.HsVar $ Hs.UnQual $ name)
+        app_with_handle_and_structure f = Hs.HsApp (Hs.HsApp (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent f)
+                                                             (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "handle"))
+                                                   (Hs.HsVar $ Hs.UnQual $ Hs.HsIdent "structure")
+
+
 hs_con_exp qDcon exp_args = foldl Hs.HsApp exp_con exp_args
   where exp_con = (Hs.HsCon $ Hs.UnQual $ Hs.HsIdent $ snd qDcon)
 
--- DbRadtime.hs
+++ DbRadtime.hs
@@ -18,14 +18,17 @@ import Text.Blaze.Renderer.Utf8 (renderHtml)
 
 import Database.SQLite
 
-import Radtime (Char_incrementalised, Int_incrementalised, ZMZN_incrementalised (..), processRequest)
+import Radtime (Char_incrementalised, Int_incrementalised, ZMZN_incrementalised (..), processRequest, applyInputChange)
 
 data DbAddressComponent = DbAddressComponent String Int
 
 type DbAddress = [DbAddressComponent]
 
 class DbIncrementalised incrementalised where
-  applyDbInputChange :: incrementalised -> DbAddress -> IO ()
+  applyDbInputChange :: SQLiteHandle -> DbStructure -> incrementalised -> DbAddress -> IO ()
+
+class DbInitialise base where
+  setInitialValue :: SQLiteHandle -> DbStructure -> base -> IO [(String, String)]
 
 
 data DbStrategy = Inline | Separate
@@ -42,23 +45,46 @@ appendAddress address constructor fieldIndex = address ++ newPiece
 
 rootAddress = []
 
+atomicApplyDbInputChange reader shower handle structure change relative_address = let address@(table, column, condition) = find_absolute_address structure relative_address
+                                                                                   in do putStrLn $ build_select column table condition
+                                                                                         r <- execStatement handle $ build_select column table condition
+                                                                                         let v = case r of
+                                                                                                   Left e -> error $ "Couldn't select " ++ show address ++ ": " ++ e
+                                                                                                   Right ((((column_name,value):_):_):_) -> value
+                                                                                                   Right empty -> error $ "Couldn't select " ++ show address ++ ": was empty"
+                                                                                         let new_v = applyInputChange change (reader v)
+                                                                                         let update_sql = build_update table column (shower new_v) condition
+                                                                                         putStrLn $ update_sql
+                                                                                         execStatement_ handle update_sql >>= failIfError
+              
+
 instance DbIncrementalised Char_incrementalised where
-  applyDbInputChange change address = error "not implemented"
+  applyDbInputChange = atomicApplyDbInputChange (\a -> (head a) :: Char) (\a -> show a)
 
+instance DbInitialise Char where
+  setInitialValue handle structure c = return [("Char", show c)]
+  
 instance DbIncrementalised Int_incrementalised where
-  applyDbInputChange change address = error "not implemented"
+  applyDbInputChange = atomicApplyDbInputChange (\(Int a) -> (fromIntegral a) :: Int) (\a -> show a)
 
+instance DbInitialise Int where
+  setInitialValue handle structure n = return [("Int", show n)]
+  
 instance (DbIncrementalised elem_incrementalised) => 
             DbIncrementalised (ZMZN_incrementalised elem elem_incrementalised) where
-  applyDbInputChange (ZMZN_incrementalised) address = error "not implemented"
-  applyDbInputChange (ZC_incrementalised elem_incrementalised list_incrementalised) address = do
-    applyDbInputChange elem_incrementalised (appendAddress address "ZC_incrementalised" 0)
-  applyDbInputChange change address = error "not implemented"
+  applyDbInputChange handle structure (ZMZN_incrementalised) address = error "not implemented"
+  applyDbInputChange handle structure (ZC_incrementalised elem_incrementalised list_incrementalised) address = do
+    applyDbInputChange handle structure elem_incrementalised (appendAddress address "ZC_incrementalised" 0)
+  applyDbInputChange handle structure change address = error "not implemented"
 
-prepareDb :: DbStructure -> IO SQLiteHandle
-prepareDb structure = do
+instance DbInitialise [elem] where
+  setInitialValue handle structure n = return []
+
+prepareDb structure initial_value = do
   handle <- openConnection "db"
   mapM_ (prepareTable handle structure) structure
+  initial_value_columns <- setInitialValue handle structure initial_value
+  void $ perform_insert handle "AppState" initial_value_columns
   return handle
 
 failIfError Nothing = return ()
@@ -76,27 +102,60 @@ prepareTable handle structure (name, strategy, cons) = execStatement_ handle ("C
         con_member_columns con_name member n
            | member_strategy == Inline = inline_column member_name member_cons n
            | member_strategy == Separate = separate_column member_name n
-          where (member_name, member_strategy, member_cons) = lookup_in_structure member
+          where (member_name, member_strategy, member_cons) = lookup_in_structure structure member
         separate_column member_name n = prepend_to_names (member_name ++ "__" ++ (show n)) $ [("type", "text"), ("id", "integer")]
         inline_column member_name member_cons n = prepend_to_names (member_name ++ "__" ++ (show n)) $ concat $ map inline_column_con member_cons
         inline_column_con (con_name, members) = prepend_to_names con_name $ con_columns (con_name, members)
-        lookup_in_structure "" = ("anything", Separate, [])
-        lookup_in_structure s = case find (\(a, _, _) -> a == s) structure of
-                                   Just a -> a
-                                   otherwise -> error ("Couldn't find structure for " ++ s)
-        prepend_to_names :: String -> [(String, String)] -> [(String, String)]
-        prepend_to_names s = map (\(a, b) -> (s ++ "__" ++ a, b))
 
-         
+lookup_in_structure structure "" = ("anything", Separate, [])
+lookup_in_structure structure s = case find (\(a, _, _) -> a == s) structure of
+                           Just a -> a
+                           otherwise -> error ("Couldn't find structure for " ++ s)
 
-app parse_request state incrementalised_state_function representationFunction request = do
+prepend_to_names :: String -> [(String, String)] -> [(String, String)]
+prepend_to_names s = map (\(a, b) -> (s ++ "__" ++ a, b))
+
+
+setInitialValue' :: SQLiteHandle -> DbStructure -> String -> [IO [(String, String)]] -> IO [(String, String)]
+setInitialValue' handle structure type_name actions = 
+  let (member_name, member_strategy, member_cons) = lookup_in_structure structure type_name
+   in case member_strategy of
+        Inline -> do columns <- sequence actions
+                     return $ concat $ zipWith (\ns n -> map (\(name, value) -> (name ++ "__" ++ (show n), value)) ns) columns [0..]
+        otherwise -> return []
+
+perform_insert handle table values = let cols_sql = concat $ intersperse ", " $ map fst values
+                                         vals_sql = concat $ intersperse ", " $ map (\a -> "'" ++ snd a ++ "'") values
+                                         sql = "INSERT INTO " ++ table ++ " (" ++ cols_sql ++ ") VALUES (" ++ vals_sql ++ ")"
+                                      in do putStrLn sql
+                                            execStatement_ handle sql
+
+build_select a b "" = "SELECT " ++ a ++ " FROM " ++ b
+build_select a b c = (build_select a b "") ++ " WHERE " ++ c
+
+build_update table column value "" = "UPDATE " ++ table ++ " SET " ++ column ++ " = " ++ value
+build_update table column value condition = build_update table value column "" ++ " WHERE " ++ condition
+
+
+find_absolute_address _ [] = error "find_absolute_address: no root"
+find_absolute_address structure ((DbAddressComponent rootCon fieldIndex):address) = foldl (find_absolute_address' structure) initial address
+  where initial = (rootCon, (constructor !! fieldIndex) ++ "__" ++ (show fieldIndex), "")
+        (_, _, constructors) = lookup_in_structure structure rootCon
+        constructor = snd $ head $ constructors -- should be a lookup based on part of fieldIndex, not currently captured
+
+find_absolute_address' structure (table, field, condition) (DbAddressComponent addr_con field_index)
+    | strategy == Inline = (table, field ++ "__" ++ (show field_index), condition)
+    | strategy == Separate = (name, "", "id = (" ++ (build_select (field ++ "__" ++ (show field_index)) table condition) ++ ")") 
+  where (name, strategy, constructors) = lookup_in_structure structure addr_con
+
+app handle structure parse_request state incrementalised_state_function representationFunction request = do
   request_body_chunks <- EL.consume
   let request_body_query = parseQuery $ B.concat request_body_chunks
   let maybe_input_change = processRequest parse_request request request_body_query
   case maybe_input_change of
     Nothing             -> return ()
     (Just input_change) -> let output_change = incrementalised_state_function input_change
-                            in tryIO $ applyDbInputChange output_change rootAddress
+                            in tryIO $ applyDbInputChange handle structure output_change rootAddress
   let response = responseLBS
               status200
               [("Content-Type", B8.pack "text/html")]
@@ -105,7 +164,7 @@ app parse_request state incrementalised_state_function representationFunction re
 
 runAppDb parse_request initial_state db_structure incrementalized_state_function page_view = do
     putStrLn $ "http://localhost:8080/"
-    prepareDb db_structure
-    run 8080 $ app parse_request initial_state incrementalized_state_function page_view 
+    handle <- prepareDb db_structure initial_state
+    run 8080 $ app handle db_structure parse_request initial_state incrementalized_state_function page_view 
 
 
