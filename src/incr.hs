4e913a611a0b282428f327c67095c0ebfcdc2180
Iain McCoy <iain@mccoy.id.au>
2012-03-17 17:27:59 +1100

    more general incr.hs tool

--- src/incr.hs
+++ src/incr.hs
@@ -1,5 +1,8 @@
-import Prelude hiding (FilePath)
+import Prelude hiding (FilePath, concat)
 import Data.List (last)
+import qualified Data.List as L
+import Data.Maybe
+import Debug.Trace
 import System (getArgs)
 
 import Filesystem.Path.CurrentOS hiding (fromText)
@@ -11,13 +14,30 @@ import Shelly
 incr :: FilePath -> FilePath -> ShIO ()
 incr destDirName srcCoreFileName = do
   let baseName = basename srcCoreFileName
-  let incrementalizedCoreFileName = append destDirName $ append (dirname srcCoreFileName) (decodeString $ encodeString baseName ++ "-incr.hcr")
+  let incrementalizedCoreFileName = append destDirName $ append (directory srcCoreFileName) (decodeString $ encodeString baseName ++ "-incr.hcr")
   let incrementalizedHsFileName = replaceExtension incrementalizedCoreFileName (ST.pack "hs")
+  liftIO $ putStrLn $ show (srcCoreFileName, baseName, incrementalizedCoreFileName, incrementalizedHsFileName)
   run_ (fromText $ T.pack "Incrementalizer") $ map toTextUnsafe [srcCoreFileName, incrementalizedCoreFileName]
   run_ (fromText $ T.pack "hcr2hs") $ map toTextUnsafe [incrementalizedCoreFileName, incrementalizedHsFileName]
 
+expandFileNames fileNames = mapM expandFileName fileNames >>= return . L.concat
+
+expandFileName fileName = do
+  isDir <- test_d fileName
+  if isDir
+    then expandDir fileName
+    else return [fileName]
+
+expandDir dirName = do
+  paths <- find dirName
+  cwd <- pwd >>= (\x -> return $ T.append (toTextUnsafe x) (T.pack "/"))
+  return $ map (relativize cwd) $ filter (\x -> FP.extension x == (Just $ ST.pack "hcr")) paths
+  where relativize cwd fileName = fromText $ fromJust $ T.stripPrefix cwd (toTextUnsafe fileName)
+
 main = do
   args <- getArgs
-  let (destDirName:srcCoreFileNames) = reverse args
+  let args' = if length args == 1 then ".":args else args
+  let (destDirName:srcCoreFileNames) = reverse args'
   shelly $ verbosely $ do
-    mapM_ (incr (fromText $ T.pack destDirName)) (map decodeString srcCoreFileNames)
+    srcCoreFileNames' <- expandFileNames $ map decodeString $ srcCoreFileNames
+    mapM_ (incr (fromText $ T.pack destDirName)) srcCoreFileNames'
