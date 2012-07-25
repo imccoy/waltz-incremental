8a245112a31f51a207bcef7355faba24b047c2ce
Iain McCoy <iain@mccoy.id.au>
2012-07-25 16:27:26 +1000

    Revert "Turns out this was a rabbit hole."
    Moving towards an approach where incrementalised functions take both the
    old value and the change. The idea is still that they shouldn't touch
    the old value if they don't need to, and should still return a change.
    
    This reverts commit 721c77f0a2fca911c59107a3cb3b519a02c07bcc.

--- demo/B.hs
+++ demo/B.hs
@@ -1,50 +1,99 @@
 module B where
+import Prelude hiding (map, filter, (++))
 import Inctime
 import InctimeHtml
 
-data TryInt =  TryIntP Int
 
-data TryList a = EmptyTryList | ConsTryList a (TryList a)
+filter f []                 = []
+filter f (x:xs) | f x       = x:(filter f xs)
+                | otherwise = filter f xs
 
-data AppState = AppState { appStateWordsLength :: Int
+map f [] = []
+map f (x:xs) = (f x):(map f xs)
+                       
+[] ++ a = a
+(x:xs) ++ a = x:(xs ++ a)
+
+
+
+data WordDefinitions = WordDefinitions String [String]
+
+data AppState = AppState { appStateNumWords :: Int
+                         , appStateNumDefinitions :: Int
                          , appStateWords :: [String]
-                         , appStateMostRecentWord :: String
+                         , appStateDefinitions :: [WordDefinitions]
                          }
 
-thing True = "yes"
-thing False = "no"
+data Input = NewWordInput String
+           | NewDefinitionInput String String
+isNewWordInput i = case i of
+                     (NewWordInput _) -> True
+                     otherwise        -> False
+isNewDefinitionInput i = case i of
+                           (NewDefinitionInput _ _) -> True
+                           otherwise                -> False
+newWordInputs = filter isNewWordInput
+newDefinitionInputs = filter isNewDefinitionInput
+definitionInputsFor w inputs = filter (inputContainsWord w) (newDefinitionInputs inputs)
+wordFrom (NewWordInput w) = w
+wordFrom (NewDefinitionInput w _) = w
+inputContainsWord word i = wordFrom i == word
+definitionFrom i = case i of 
+                     (NewDefinitionInput _ d) -> d
+                     otherwise                -> error "No definition"
 
-app_state words = AppState { appStateWordsLength = words_length words
-                           , appStateWords = words
-                           , appStateMostRecentWord = head words }
+wordDefinitions (WordDefinitions _ ds) = ds
 
-words_length [] = 0
-words_length (w:ws) = (length w) + (words_length ws)
+app_state inputs
+  = AppState { appStateNumWords = length (words inputs)
+             , appStateNumDefinitions = elems_length (map wordDefinitions (definitions inputs))
+             , appStateWords = words inputs
+             , appStateDefinitions = definitions inputs
+             }
+  where words inputs = map wordFrom (newWordInputs inputs)
+        definitions inputs = let f inputs w = word_definitions w inputs
+                              in map (f inputs) (words inputs)
+        word_definitions w inputs = WordDefinitions w (map definitionFrom (definitionInputsFor w inputs))
+
+elems_length [] = 0
+elems_length (w:ws) = (length w) + (elems_length ws)
 
 -- if we call words_length_incrementalised directly from un-incrementalised
 -- code, we run into trouble because we can't pass the typeclass argument. By
 -- calling a specialised version, the machinery generates code to pick the
 -- relevant typeclass argument.
 words_length_stringy :: [String] -> Int
-words_length_stringy = words_length
+words_length_stringy = elems_length
 
 
 page_view state = domElem "div" [
-  domElem "h1" [tElem "The Word Monster"],
+  domElem "h1" [tElem "The Urbane Dictionary"],
   domElem "p" [
-    tElem "I'm the word monster. Words are delicious! So far today, I've eaten ",
-    domElem "span" [
-      tElemB (IncBox show (appStateWordsLength state))
-    ],
-    tElem " letters."
+    tElem ("I'm the urbane dictionary. The definitions are classy, even when" ++
+           "the words are not."),
+    tElemB (IncBox (\x -> show x ++ " words.") (appStateNumWords state)),
+    tElemB (IncBox (\x -> show x ++ " definitions.") (appStateNumDefinitions state))
   ],
-  domElem "p" [
-    tElem "May I please have some more? I just had a marvellous '",
-    tElem (appStateMostRecentWord state),
-    tElem "'"
-  ],
+  domElem "div" (map (\(WordDefinitions word definitions) ->
+                        domElem "div" [
+                          tElem word,
+                          domElem "div" (map (\definition ->
+                                                 domElem "div" [
+                                                   tElem definition
+                                                 ]
+                                             ) definitions)
+                        ]
+                     )
+                     (appStateDefinitions state))
+  ,
   elemA "form" [Attr "method" "post"] [
     elemA "input" [Attr "name" "word"] [],
-    elemA "input" [Attr "type" "submit", Attr "value" "Feed the monster!"] []
+    elemA "input" [Attr "name" "definition"] [],
+    elemA "input" [Attr "name" "action",
+                   Attr "type" "submit",
+                   Attr "value" "Add Definition"] [],
+    elemA "input" [Attr "name" "action", 
+                   Attr "type" "submit",
+                   Attr "value" "Add Word"] []
   ]
  ]
--- src/Lookups.hs
+++ src/Lookups.hs
@@ -236,6 +236,21 @@ incBoxTyCon
                                    "IncBox"
                                    OccName.tcName
 
+noIncLamId :: TypeLookupM Id
+noIncLamId
+  = lookupAndConvertInctimeTyThing tyThingId
+                                   "noIncLamId"
+                                   "noIncLam"
+                                   OccName.varName
+
+noIncAppId :: TypeLookupM Id
+noIncAppId
+  = lookupAndConvertInctimeTyThing tyThingId
+                                   "noIncAppId"
+                                   "noIncApp"
+                                   OccName.varName
+
+
 
 lookupPreludeFn :: String -> String -> TypeLookupM Var
 lookupPreludeFn m n = do
